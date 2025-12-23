23bd18cd2c1ca983ddc69f75c1f65fdab6e06929
Zakaria <zakariakebour@gmail.com>
2025-12-23 23:49:12 +0100

    Add files via upload

---
+++ tienda_online/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ZAPSTORE | España</title>
+    <link rel="stylesheet" href="estilos/index.css">
+</head>
+<body>
+    <!--Aqui colocamos la etiqueda del video dentro del header-->
+    <header>
+        <div class="contenedor-video">
+        <video autoplay muted playsinline loop>
+            <source src="videos/video.mp4" type="video/mp4">
+        </video>
+        <h1 class="titulo">ZAPSTORE</h1>
+        <h2 class="frase">El paso perfecto empieza aquí</h2>
+        <a href="" class="iniciar-sesion">Iniciar sesión</a>
+        </div>
+    </header>
+    <!--Colocamos el main con articulos-->
+    <main>
+    <article>
+        <h1 class="zap">ZAP</h1>
+        <h1 class="store">STORE</h1>
+    </article>
+    <div class="logos">
+        <img src="logos/adidaslogo-removebg-preview.png" alt="adidas">
+        <img src="logos/nikelogo-removebg-preview.png" alt="nike">
+        <img src="logos/converselogo-removebg-preview.png" alt="vans">
+        <img src="logos/vanslogo-removebg-preview.png" alt="converse">
+    </div>
+    </main>
+    <script></script>
+</body>
+</html>
+
+<?php
+?>
